03fbb515f719cb1b0dc49ceed24450e43c75df0d
whoiscc <correctizer@gmail.com>
2019-06-21 10:38:38 +0800

    Update AngelSerializer
    Signed-off-by: whoiscc <correctizer@gmail.com>

--- mywing/angel/serializers.py
+++ mywing/angel/serializers.py
@@ -2,9 +2,13 @@
 
 from rest_framework import serializers
 from mywing.angel.models import Angel
+from mywing.task.serializers import TaskSerializer
 
 
 class AngelSerializer(serializers.ModelSerializer):
+    owned_tasks = TaskSerializer(many=True, read_only=True)
+    helped_tasks = TaskSerializer(many=True, read_only=True)
+
     class Meta:
         model = Angel
         fields = ('id', 'real_name', 'contribution', 'owned_tasks', 'helped_tasks')
