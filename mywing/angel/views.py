33a171397e52c4bef73e8aab7138bc7154c1f57c
whoiscc <correctizer@gmail.com>
2019-07-02 21:38:16 +0800

    Fix several minor issues
    Signed-off-by: whoiscc <correctizer@gmail.com>

--- mywing/angel/views.py
+++ mywing/angel/views.py
@@ -6,6 +6,7 @@ from rest_framework.decorators import api_view
 from rest_framework.authtoken.models import Token
 from cas import CASClient
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AnonymousUser
 from urllib.parse import unquote
 from mywing.angel.serializers import AngelSerializer, CASLoginSerializer
 from mywing.angel.models import Angel
@@ -27,6 +28,8 @@ class CASLoginView(generics.GenericAPIView):
     queryset = Angel.objects.all()
 
     def post(self, request):
+        if not isinstance(request.user, AnonymousUser):
+            raise serializers.ValidationError('already login')
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         data = serializer.validated_data
